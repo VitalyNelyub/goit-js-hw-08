50bc115a42e4cb892382ecd45e8175edc3d7d218
Vitaly <nelyub86@ukr.net>
2023-01-24 14:18:57 +0200

    test #1 finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import galleryItems from '../js/gallery-items';
+console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryCards = createGalleryItem(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryCards);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>;
+</div>
+  `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
