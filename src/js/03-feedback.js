81c6c8edfceebec7820d7fcb077b7bcbeeb1b34d
Vitaly <nelyub86@ukr.net>
2023-01-27 10:21:13 +0200

    Update #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,38 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'feedback-form-state';
-
-const formData = {
-
-}
-
 const refs = {
   form: document.querySelector('.feedback-form'),
-//   inputEmail: document.querySelector('.feedback-form input'),
+  inputEmail: document.querySelector('.feedback-form input'),
   textarea: document.querySelector('.feedback-form textarea'),
 };
 
 refs.form.addEventListener('submit', submitForm);
-// refs.form.addEventListener('input', inputAllFormData);
-refs.textarea.addEventListener('input', throttle(inputTextarea, 500));
+refs.form.addEventListener('input', throttle(inputAllFormData, 500));
 
-savedDataForm();
+const STORAGE_KEY = 'feedback-form-state';
 
-function inputAllFormData(e) {    
-    formData[e.target.name] = e.target.value;
-    console.log(formData);
+let formData = {};
 
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+saveDataForm();
+
+function inputAllFormData(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
 
 function submitForm(e) {
   e.preventDefault();
   e.currentTarget.reset();
-  localStorage.removeItem('STORAGE_KEY');
+  localStorage.removeItem(STORAGE_KEY);
 }
 
-function inputTextarea(e) {
-  const message = e.target.value;
-  console.log(message);
-  localStorage.setItem('STORAGE_KEY', message);
-}
-
-function savedDataForm() {
-    const savedMassege = localStorage.getItem('STORAGE_KEY');
-
-  if (savedMassege) {
-    refs.textarea.value = savedMassege;
+function saveDataForm() {
+//   localStorage.getItem(STORAGE_KEY);
+  const message = localStorage.getItem(STORAGE_KEY);
+  const messageJSON = JSON.parse(message);
+  if (messageJSON) {
+    inputEmail.value = messageJSON.email;
+    textarea.value = messageJSON.message;
   }
 }
-
-
